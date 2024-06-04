a5e87c2e20fbc5eb5b8897da85cb854fe6a9511b
takuowake <takuowake@gmail.com>
2024-06-04 20:57:17 +0900

    ホーム画面の追加

---
+++ lib/home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/views/settings_screen.dart';
+import 'package:todo_list/views/todo_list_screen.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      children: [
+        SettingsScreen(),
+        TodoListScreen(),
+      ],
+    );
+  }
+}
