39dc38ab0d444e8bb98145364d3512975ad184c6
takuowake <takuowake@gmail.com>
2024-06-05 08:11:13 +0900

    スワイプ機能を追加

--- lib/home.dart
+++ lib/home.dart
@@ -6,7 +6,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return PageView(
-      children: [
+      children: <Widget>[
         SettingsScreen(),
         TodoListScreen(),
       ],
