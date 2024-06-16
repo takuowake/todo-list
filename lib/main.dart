0f28273b5ccda5337013504e8d8b3ace8331a513
takuowake <takuowake@gmail.com>
2024-06-17 08:55:07 +0900

    タイトルを削除

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'To-Do App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
