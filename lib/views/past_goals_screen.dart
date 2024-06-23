9461090f349015d8add53d28227a4b349d6a597f
takuowake <takuowake@gmail.com>
2024-06-23 20:08:47 +0900

    過去の目標一覧画面を追加

--- lib/views/past_goals_screen.dart
+++ lib/views/past_goals_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
+import 'package:grouped_list/grouped_list.dart';
 import '../controllers/goal_provider.dart';
 import '../models/goal_model.dart';
 
@@ -13,25 +14,31 @@ class PastGoalsScreen extends ConsumerWidget {
       appBar: AppBar(
         title: Text('過去の目標一覧'),
       ),
-      body: ListView.builder(
-        itemCount: pastGoals.length,
-        itemBuilder: (context, index) {
-          final goal = pastGoals[index];
-          return Card(
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            child: ListTile(
-              title: Text(goal.title),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text('作成日: ${DateFormat('yyyy/MM/dd').format(goal.createdTime)}'),
-                  Text('完了日: ${goal.completionDate != null ? DateFormat('yyyy/MM/dd').format(goal.completionDate!) : '未完了'}'),
-                ],
-              ),
-              trailing: Icon(goal.isCompleted ? Icons.check_circle : Icons.cancel),
+      body: GroupedListView<Goal, String>(
+        elements: pastGoals,
+        groupBy: (goal) => DateFormat('yyyy/MM/dd').format(goal.completionDate ?? goal.createdTime),
+        groupSeparatorBuilder: (String groupByValue) => Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            groupByValue,
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
+        itemBuilder: (context, Goal goal) {
+          return ListTile(
+            title: Text(goal.title),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('作成日: ${DateFormat('yyyy/MM/dd').format(goal.createdTime)}'),
+                Text('完了日: ${goal.completionDate != null ? DateFormat('yyyy/MM/dd').format(goal.completionDate!) : '未完了'}'),
+              ],
             ),
+            trailing: Icon(goal.isCompleted ? Icons.check_circle : Icons.cancel),
           );
         },
+        useStickyGroupSeparators: true, // グループセパレータを固定する
+        floatingHeader: true, // フローティングヘッダを有効にする
       ),
     );
   }
