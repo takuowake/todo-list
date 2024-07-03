b9c2ed1e53e7b3adc1a5d6c35af179335dad191d
takuowake <takuowake@gmail.com>
2024-07-04 07:55:06 +0900

    文言変更

--- lib/views/goal_list_screen.dart
+++ lib/views/goal_list_screen.dart
@@ -180,7 +180,7 @@ class _GoalListScreenState extends ConsumerState<GoalListScreen> {
                       color: showCompletedTasks ? Colors.lightBlueAccent : Colors.black,
                     ),
                     label: Text(
-                      showCompletedTasks ? '目標を隠す' : '24時間以内に設定した完了済みの目標を見る',
+                      showCompletedTasks ? '目標を隠す' : '24時間以内の完了済み目標を見る',
                       style: TextStyle(
                         color: showCompletedTasks ? Colors.lightBlueAccent : Colors.black,
                       ),
