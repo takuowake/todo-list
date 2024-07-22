088dd236a02ac9cd362aba7932a31b98eebfc0fe
takuowake <takuowake@gmail.com>
2024-07-23 01:24:09 +0900

    サービスにuseridを追加

--- lib/services/goal_service.dart
+++ lib/services/goal_service.dart
@@ -9,22 +9,18 @@ class GoalService {
 
   GoalService(this._repository);
 
-  // 目標リストを取得するメソッド
-  Future<List<Goal>> getGoals() async {
-    return await _repository.fetchGoals();
+  Future<List<Goal>> getGoals(String userId) async {
+    return await _repository.fetchGoals(userId);
   }
 
-  // 新しい目標を追加するメソッド
   Future<void> addGoal(Goal goal) async {
     await _repository.addGoal(goal);
   }
 
-  // 目標を更新するメソッド
   Future<void> updateGoal(Goal goal) async {
     await _repository.updateGoal(goal);
   }
 
-  // 目標を削除するメソッド
   Future<void> deleteGoal(String id) async {
     await _repository.deleteGoal(id);
   }
