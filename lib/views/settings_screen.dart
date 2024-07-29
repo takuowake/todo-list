8bac80d0e2042a0f8ccc1809ad21848c0459326f
takuowake <takuowake@gmail.com>
2024-07-30 08:57:52 +0900

    設定画面のUI作成

--- lib/views/settings_screen.dart
+++ lib/views/settings_screen.dart
@@ -6,6 +6,7 @@ import 'package:goal_list/views/contact_us_screen.dart';
 import 'package:goal_list/views/past_goals_screen.dart';
 import 'package:goal_list/views/privacy_policy_screen.dart';
 import 'package:goal_list/views/terms_of_service_screen.dart';
+import 'package:goal_list/views/widgets/custom_list_tile.dart';
 
 class SettingsScreen extends StatelessWidget {
   final VoidCallback onBackPressed;
@@ -33,42 +34,27 @@ class SettingsScreen extends StatelessWidget {
           ),
         ),
         child: ListView(
+          padding: EdgeInsets.only(top: 100.0, left: 10, right: 10), // 全体の余白を追加
           children: [
-            ListTile(
-              title: Text('過去の目標一覧'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PastGoalsScreen()),
-                );
-              },
+            buildCustomListTile(
+              context: context,
+              title: '過去の目標一覧',
+              destination: PastGoalsScreen(),
             ),
-            ListTile(
-              title: Text('利用規約'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => TermsOfServiceScreen()),
-                );
-              },
+            buildCustomListTile(
+              context: context,
+              title: '利用規約',
+              destination: TermsOfServiceScreen(),
             ),
-            ListTile(
-              title: Text('プライバシーポリシー'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => PrivacyPolicyScreen()),
-                );
-              },
+            buildCustomListTile(
+              context: context,
+              title: 'プライバシーポリシー',
+              destination: PrivacyPolicyScreen(),
             ),
-            ListTile(
-              title: Text('お問い合わせ'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ContactUsScreen()),
-                );
-              },
+            buildCustomListTile(
+              context: context,
+              title: 'お問い合わせ',
+              destination: ContactUsScreen(),
             ),
           ],
         ),
