4d2007ce8cc0a1b260b0b3f7e44791abc27d712a
takuowake <takuowake@gmail.com>
2024-07-31 09:07:37 +0900

    編集画面のレイアウトを変更

--- lib/views/edit_goal_screen.dart
+++ lib/views/edit_goal_screen.dart
@@ -16,27 +16,52 @@ class EditGoalScreen extends ConsumerWidget {
     final textController = TextEditingController(text: goal.title);
 
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
         title: Text('目標を編集'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            TextField(
-              controller: textController,
-              decoration: InputDecoration(labelText: '目標'),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background.png'), // 背景画像を追加
+                fit: BoxFit.cover,
+              ),
             ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                ref.read(goalListProvider.notifier).edit(goal.id, textController.text);
-                Navigator.pop(context);
-              },
-              child: Text('保存する'),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: [
+                  TextField(
+                    controller: textController,
+                    decoration: InputDecoration(labelText: '目標'),
+                  ),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shadowColor: Colors.transparent,
+                      elevation: 0,
+                    ),
+                    onPressed: () {
+                      ref.read(goalListProvider.notifier).edit(goal.id, textController.text);
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      style: TextStyle(color: Colors.black),
+                      '保存する',
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
