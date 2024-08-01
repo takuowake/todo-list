a39350b543ed49c3f33f3f402727bbac20b25e53
takuowake <takuowake@gmail.com>
2024-08-01 09:26:30 +0900

    過去の目標一覧に表示するためにフィルタリングをproviderに追加

--- lib/providers/goal_provider.dart
+++ lib/providers/goal_provider.dart
@@ -10,6 +10,7 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class GoalListNotifier extends StateNotifier<List<Goal>> {
   final GoalService _service;
+  List<Goal> _allGoals = []; // 全ての目標を保持するリストを初期化
 
   GoalListNotifier(this._service) : super([]) {
     loadGoals();
@@ -19,43 +20,60 @@ class GoalListNotifier extends StateNotifier<List<Goal>> {
   Future<void> loadGoals() async {
     final userId = await UserUtils.getUserId();
     final goals = await _service.getGoals(userId);
-    state = goals;
+    _allGoals = goals;
+    state = _filterActiveGoals(goals);
+  }
+
+  // 目標のリストから期限切れのものをフィルタリングするメソッド
+  List<Goal> _filterActiveGoals(List<Goal> goals) {
+    final now = DateTime.now();
+    return goals.where((goal) => goal.updatedTime.add(Duration(hours: 24)).isAfter(now)).toList();
+  }
+
+  // 過去の目標リストを取得するメソッド
+  List<Goal> getPastGoals() {
+    final now = DateTime.now();
+    return _allGoals.where((goal) => goal.updatedTime.add(Duration(hours: 24)).isBefore(now)).toList();
   }
 
   // 新しい目標を追加するメソッド
   void add(Goal goal) async {
     await _service.addGoal(goal);
-    state = [...state, goal];
+    _allGoals.add(goal);
+    state = _filterActiveGoals(_allGoals);
   }
 
   // 目標を編集するメソッド
   void edit(String id, String title) async {
-    final index = state.indexWhere((goal) => goal.id == id);
+    final index = _allGoals.indexWhere((goal) => goal.id == id);
     if (index == -1) return;
 
-    final updatedGoal = state[index].copyWith(title: title, updatedTime: DateTime.now());
+    final updatedGoal = _allGoals[index].copyWith(title: title, updatedTime: DateTime.now());
     await _service.updateGoal(updatedGoal);
-    state = [...state]..[index] = updatedGoal;
+    _allGoals[index] = updatedGoal;
+    state = _filterActiveGoals(_allGoals);
   }
 
   // 目標を削除するメソッド
   void remove(String id) async {
     await _service.deleteGoal(id);
-    state = state.where((goal) => goal.id != id).toList();
+    _allGoals = _allGoals.where((goal) => goal.id != id).toList();
+    state = _filterActiveGoals(_allGoals);
   }
 
   // 目標の完了状態を切り替えるメソッド
   void toggleComplete(String id) async {
-    final index = state.indexWhere((goal) => goal.id == id);
+    final index = _allGoals.indexWhere((goal) => goal.id == id);
     if (index == -1) return;
 
-    final updatedGoal = state[index].copyWith(
+    final updatedGoal = _allGoals[index].copyWith(
       isCompleted: !state[index].isCompleted,
       updatedTime: DateTime.now(),
       completionDate: state[index].isCompleted ? null : DateTime.now(),
     );
     await _service.updateGoal(updatedGoal);
-    state = [...state]..[index] = updatedGoal;
+    _allGoals[index] = updatedGoal;
+    state = _filterActiveGoals(_allGoals);
   }
 }
 
